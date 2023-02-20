459172859a7bb38984b0bb1f43758b17f13c2f1f
Florian Lehne <f.lehne@viamedici.de>
2023-02-20 14:48:56 +0100

    Added eqNullable extension.

--- src/Eq.ts
+++ src/Eq.ts
@@ -5,3 +5,14 @@ import * as Eq from "fp-ts/Eq";
 export function combine<A, B>(a: Eq.Eq<A>, b: Eq.Eq<B>): Eq.Eq<A & B> {
     return Eq.fromEquals<A & B>((x, y) => a.equals(x, y) && b.equals(x, y));
 }
+
+export function eqNullable<T>(eq: Eq.Eq<T>): Eq.Eq<T | null | undefined> {
+    return Eq.fromEquals<T | null | undefined>((x, y) => {
+            const xIsNull = (x === null || x === undefined);
+            const yIsNull = (y === null || y === undefined);
+
+            return (xIsNull && yIsNull)
+                || (!xIsNull && !yIsNull && eq.equals(x, y));
+        }
+    )
+}
